4f989714d09904bd0a1caa74b5354f80d74d1de7
kirill.garykin <kirill.garykin@gmail.com>
2017-04-28 10:50:57 +0300

    initial :: IdentityServer4.Quickstart.UI

--- Volo.IdentityServer.Web/Startup.cs
+++ Volo.IdentityServer.Web/Startup.cs
@@ -1,21 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Reflection;
 using Volo.IdentityServer.Data;
-using Volo.IdentityServer.Data.IdentityModels;
-using Microsoft.EntityFrameworkCore;
-using IdentityServer4.EntityFramework.DbContexts;
-
-using System.Linq;
-using IdentityServer4.EntityFramework.Mappers;
-using Microsoft.AspNetCore.Identity;
-using Volo.IdentityServer.Data.TestConfiguration;
 using Volo.IdentityServer.Data.Extensions;
-
-using Microsoft.AspNetCore.Http;
 
 namespace Volo.IdentityServer.Web
 {
@@ -25,30 +14,8 @@ namespace Volo.IdentityServer.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //const string connectionString = Constants.DevConnectionString;
-            ////  @"Data Source=UADP16WS\SQLEXPRESS;database=VoloIdentityServer;trusted_connection=yes;";
-
-            //var migrationsAssembly = typeof(ApplicationIdentityDbContext).GetTypeInfo().Assembly.GetName().Name;
-
-            //// ASP.NET Identity DbContext
-            //services.AddDbContext<ApplicationIdentityDbContext>(options => options.UseSqlServer(connectionString));
-
-            //// ASP.NET Identity Registrations            
-            //services.AddIdentity<User, Role>().AddEntityFrameworkStores<ApplicationIdentityDbContext>();
-
-            //services.AddIdentityServer()
-            //    .AddOperationalStore(
-            //        builder => builder.UseSqlServer(connectionString, options => options.MigrationsAssembly(migrationsAssembly)))
-            //    .AddConfigurationStore(
-            //        builder => builder.UseSqlServer(connectionString, options => options.MigrationsAssembly(migrationsAssembly)))
-            //    .AddAspNetIdentity<User>()
-            //    .AddTemporarySigningCredential();
-
-            //// services.AddMvc();
-
-
-
             services.RegisterSecurityServices(Constants.DevConnectionString);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,10 +23,10 @@ namespace Volo.IdentityServer.Web
         {
             loggerFactory.AddConsole();
 
-            //  if (env.IsDevelopment())
-            //  {
-            app.UseDeveloperExceptionPage();
-            //  }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
 
             // InitializeDbTestData(app);
@@ -67,8 +34,8 @@ namespace Volo.IdentityServer.Web
             app.UseIdentity();
             app.UseIdentityServer();
 
-            // app.UseStaticFiles();
-            // app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async context => { await context.Response.WriteAsync("Panda`s Identity Server!"); });
         }
